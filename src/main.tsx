d201eae64951bf04cc809fb390e2d5ba7ee38589
Alex Lecco <alexlecco24@gmail.com>
2024-04-20 20:57:44 -0300

    create 2 routes

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,35 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App.tsx'
 import './index.css'
+
+// libraries
+import {
+  createBrowserRouter,
+  RouterProvider,
+} from 'react-router-dom';
+
+// components
+import AlexSolution from './routes/AlexSolution.tsx';
+import IsaSolution from './routes/IsaSolution.tsx';
+import App from './App.tsx';
+
+const router = createBrowserRouter([
+  {
+    path: '/alex',
+    element: <AlexSolution />
+  },
+  {
+    path: '/isa',
+    element: <IsaSolution />
+  },
+  {
+    path: '/',
+    element: <App />
+  }
+])
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </React.StrictMode>,
 )
---
+++ src/routes/AlexSolution.tsx
@@ -0,0 +1,9 @@
+import '../App.css'
+
+function AlexSolution() {
+  return (
+    <></>
+  )
+}
+
+export default AlexSolution
